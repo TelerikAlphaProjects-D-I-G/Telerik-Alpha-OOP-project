f51f12d6e5f10003f2f93d7999bd52bd9de32dd3
gosho359 <yovchev1909@gmail.com>
2025-02-10 13:53:00 +0200

    class Vehicles count and available updated

--- models/vihcles.py
+++ models/vihcles.py
@@ -1,3 +1,5 @@
+from zoneinfo import available_timezones
+
 SCANIA = ["Scania",42000,8000]
 MAN = ["Man", 37000, 10000]
 ACTROS = ["Actros",  26000,13000]
@@ -10,15 +12,12 @@ class Vehicles:
         'Actros': {'capacity': 26000, 'max range': 13000, 'quantity': 15}
     }
 
-
     def __init__(self, name, vehicle_id):
         self.name = name
+        self.vehicle_id = vehicle_id
         self.capacity = Vehicles.available_vehicles[name]['capacity']
         self.max_range = Vehicles.available_vehicles[name]['max range']
-        self.vehicle_id = vehicle_id
         self.is_available = True
-
-
 
     def assign_to_work(self):
         if self.is_available:
@@ -33,16 +32,59 @@ class Vehicles:
     @staticmethod
     def truck_info(vehicle_id):
         if 1001 <= vehicle_id <= 1010:
-            vehicle_type = 'Scania'
+            return Vehicles.available_vehicles.get('Scania')
         if 1011 <= vehicle_id <= 1025:
             return Vehicles.available_vehicles.get('Man')
         if 1026 <= vehicle_id <= 1040:
-            return ACTROS
+            return Vehicles.available_vehicles.get('Actros')
+
+    @staticmethod
+    def count_vehicles_on_road(lst):
+        vehicles_on_road = {'Scania': 0, 'Man': 0, 'Actros': 0}
+        for vehicle in lst:
+            if not vehicle.is_available:
+                vehicles_on_road[vehicle.name] += 1
+
+        available_vehicles_count = {'Scania': Vehicles.available_vehicles['Scania']['quantity'] - vehicles_on_road['Scania'],
+                                    'Man': Vehicles.available_vehicles['Man']['quantity'] - vehicles_on_road['Man'],
+                                    'Actros': Vehicles.available_vehicles['Actros']['quantity'] - vehicles_on_road['Actros']}
+
+        return vehicles_on_road, available_vehicles_count
 
     def __str__(self):
-        return f'\n'.join(Vehicles.truck_info())
+        vehicle_info = Vehicles.truck_info(self.vehicle_id)
 
-print(Vehicles.truck_info(1012))
+        if vehicle_info:
+            return (f'Name: {self.name}\n'
+                    f'Vehicle ID: {self.vehicle_id}\n'
+                    f'Capacity: {self.capacity} kg\n'
+                    f'Max Range: {self.max_range} km\n'
+                    f'Status: {'Available' if self.is_available else 'Not Available'}\n'
+                    f'Total Available: {vehicle_info['quantity']} vehicles\n')
+
+        return'No vehicle information has been found.'
+
+scania_vehicle = Vehicles('Scania', 1005)
+print(scania_vehicle)
+
+man_vehicle = Vehicles('Man', 1020)
+print(man_vehicle)
+
+actros_vehicle = Vehicles('Actros', 1030)
+print(actros_vehicle)
+
+scania_vehicle.assign_to_work()
+man_vehicle.assign_to_work()
+actros_vehicle.assign_to_work()
+
+lst = [scania_vehicle, man_vehicle, actros_vehicle]
+
+vehicles_on_road, available_vehicles = Vehicles.count_vehicles_on_road(lst)
+
+
+
+print(f'Vehicles on road: {vehicles_on_road} \nAvailable vehicles: {available_vehicles}')
+
 
 
 
