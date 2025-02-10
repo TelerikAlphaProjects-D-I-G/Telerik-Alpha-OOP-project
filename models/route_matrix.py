f9a1db0c2658b7fa95f788c193e94d2e626d7585
Deni Denchev <d.d.denchev94@gmail.com>
2025-02-10 11:43:05 +0200

    route_matrix created

--- models/route_matrix.py
+++ models/route_matrix.py
@@ -1,2 +1,22 @@
-DISTANCE_MATRIX = {
-    ("SYD", "MEL"): 877, ('SYD', 'ADL'): 1376}
+
+
+DISTANCES = {
+    "SYD": {"MEL": 877, "ADL": 1376, "ASP": 2762, "BRI": 909, "DAR": 3935, "PER": 4016},
+    "MEL": {"SYD": 877, "ADL": 725, "ASP": 2255, "BRI": 1765, "DAR": 3752, "PER": 3509},
+    "ADL": {"SYD": 1376, "MEL": 725, "ASP": 1530, "BRI": 1927, "DAR": 3027, "PER": 2785},
+    "ASP": {"SYD": 2762, "MEL": 2255, "ADL": 1530, "BRI": 2993, "DAR": 1497, "PER": 2481},
+    "BRI": {"SYD": 909, "MEL": 1765, "ADL": 1927, "ASP": 2993, "DAR": 3426, "PER": 4311},
+    "DAR": {"SYD": 3935, "MEL": 3752, "ADL": 3027, "ASP": 1497, "BRI": 3426, "PER": 4025},
+    "PER": {"SYD": 4016, "MEL": 3509, "ADL": 2785, "ASP": 2481, "BRI": 4311, "DAR": 4025}
+}
+
+def my_distance(start,end):
+    if start == end:
+        return 0
+    if start or end not in DISTANCES:
+        raise ValueError("Invalid distance")
+    return  DISTANCES.get(start,{}).get(end)
+
+print(my_distance("DAR","ABC"))
+
+
