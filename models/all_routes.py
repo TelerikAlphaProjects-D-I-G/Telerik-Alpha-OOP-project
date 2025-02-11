197395b0a847a6081d647dfa1c6623f00e8f7511
Deni Denchev <d.d.denchev94@gmail.com>
2025-02-11 17:11:01 +0200

    km_distance created in class AllR

--- models/all_routes.py
+++ models/all_routes.py
@@ -1,26 +1,21 @@
 from models.route_matrix import Routes
 class AllRoutes:
 
-	# ROUTES = ['Sydney', 'Melboune', 'Adelaide', 'Perth']
-
 
 	@staticmethod
-	def km_distance(route):
-		routes1 = ["SYD", "MEL", "ADL", "PER"]
+	def route_distance(routes):
 		total_distance = 0
-		for i in range(len(routes1)):
-			current_city = routes1[i]
-			next_city = routes1[i+1]
-			if Routes.my_distance(current_city,next_city) in routes1:
-				total_distance += Routes.my_distance(current_city, next_city)
-			elif (next_city, current_city) in routes1:
-				total_distance += Routes.my_distance(current_city, next_city)
-			else:
-				print("No")
+		for i in range(len(routes)-1):
+			current_city = routes[i]
+			next_city = routes[i +1]
+			distance = Routes.my_distance(current_city,next_city)
+			if distance is None:
+				raise ValueError
+			total_distance+=distance
 
-		print(f"Routes: {'->'.join(route)}")
+		print(f"Routes: {'->'.join(routes)}")
 		print(f"Total distance: {total_distance} km")
 		return total_distance
-route = ["SYD", "MEL", "ADL", "PER"]
+course = ["DAR","SYD","MEL"]
 
-AllRoutes.km_distance(route)
+AllRoutes.route_distance(course)
