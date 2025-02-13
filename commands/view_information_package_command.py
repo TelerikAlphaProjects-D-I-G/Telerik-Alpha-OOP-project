da42a73c813e2a071115f475af964f6a6dd52558
Deni Denchev <d.d.denchev94@gmail.com>
2025-02-13 14:09:58 +0200

    parsed str to ind in package com

--- commands/view_information_package_command.py
+++ commands/view_information_package_command.py
@@ -1,13 +1,16 @@
 from core.application_data import ApplicationData
 from commands.validate_params_helpers_command import validate_params_count
-class ViewInformationAboutPackage:
+from commands.base_command import BaseCommand
+class ViewInformationAboutPackage(BaseCommand):
+
 	def __init__(self, params, app_data: ApplicationData):
 		super().__init__(app_data)
 		validate_params_count(params, 1)
 		self._params = params
 
 	def execute(self):
-		package_info_id = self._params[0]
+		super().execute(self._params)
+		package_info_id = super()._try_parse_int(self._params[0])
 		package = self._app_data.find_package_by_id(package_info_id)
 
 		return package.__str__()
