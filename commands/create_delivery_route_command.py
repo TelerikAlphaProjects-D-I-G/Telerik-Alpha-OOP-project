36ca977d9a883f596d6e3067431b99c739ad71e7
Flazer214 <ivanpustovit786@gmail.com>
2025-02-13 13:43:39 +0200

    add create route

--- commands/create_delivery_route_command.py
+++ commands/create_delivery_route_command.py
@@ -1,4 +1,11 @@
 from commands.base_command import BaseCommand
-
+from core.application_data import ApplicationData
 class CreateDeliveryRouteCommand(BaseCommand):
-    pass
+
+    def __init__(self, params, app_data: ApplicationData):
+        self._app_data = app_data
+        self._params = params
+    def execute(self):
+        all_routes = self._params
+        new_route = self._app_data.create_route(all_routes)
+        return f"{' -> '.join(new_route.cities)}"
