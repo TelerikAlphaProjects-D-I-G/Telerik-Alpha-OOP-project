d05eabfb7f10940ed3495b933148279df8ced717
Flazer214 <ivanpustovit786@gmail.com>
2025-02-14 23:22:09 +0200

    add storage with all trucks

---
+++ storage_data/storage_trucks.py
@@ -0,0 +1,284 @@
+
+TRUCKS = {
+    "1001": {
+        'model': 'Scania',
+        'capacity': 42000,
+        'max_range': 8000,
+        'quantity': 10,
+        'city': "ADL"
+    },
+    "1002": {
+        'model': 'Scania',
+        'capacity': 42000,
+        'max_range': 8000,
+        'quantity': 5,
+        'city': "SYD"
+    },
+    "1003": {
+        'model': 'Scania',
+        'capacity': 42000,
+        'max_range': 8000,
+        'quantity': 7,
+        'city': "SYD"
+    },
+    "1004": {
+        'model': 'Scania',
+        'capacity': 42000,
+        'max_range': 8000,
+        'quantity': 10,
+        'city': "ADL"
+    },
+    "1005": {
+        'model': 'Scania',
+        'capacity': 42000,
+        'max_range': 8000,
+        'quantity': 5,
+        'city': "SYD"
+    },
+    "1006": {
+        'model': 'Scania',
+        'capacity': 42000,
+        'max_range': 8000,
+        'quantity': 7,
+        'city': "SYD"
+    },
+    "1007": {
+        'model': 'Scania',
+        'capacity': 42000,
+        'max_range': 8000,
+        'quantity': 10,
+        'city': "ADL"
+    },
+    "1008": {
+        'model': 'Scania',
+        'capacity': 42000,
+        'max_range': 8000,
+        'quantity': 5,
+        'city': "SYD"
+    },
+    "1009": {
+        'model': 'Scania',
+        'capacity': 42000,
+        'max_range': 8000,
+        'quantity': 7,
+        'city': "SYD"
+    },
+    "1010": {
+        'model': 'Scania',
+        'capacity': 42000,
+        'max_range': 8000,
+        'quantity': 10,
+        'city': "ADL"
+    },
+    "1011": {
+        'model': 'Man',
+        'capacity': 37000,
+        'max_range': 10000,
+        'quantity': 5,
+        'city': "SYD"
+    },
+    "1012": {
+        'model': 'Man',
+        'capacity': 37000,
+        'max_range': 10000,
+        'quantity': 7,
+        'city': "SYD"
+    },
+    "1013": {
+        'model': 'Man',
+        'capacity': 37000,
+        'max_range': 10000,
+        'quantity': 10,
+        'city': "ADL"
+    },
+    "1014": {
+        'model': 'Man',
+        'capacity': 37000,
+        'max_range': 10000,
+        'quantity': 5,
+        'city': "SYD"
+    },
+    "1015": {
+        'model': 'Man',
+        'capacity': 37000,
+        'max_range': 10000,
+        'quantity': 7,
+        'city': "SYD"
+    },
+    "1016": {
+        'model': 'Man',
+        'capacity': 37000,
+        'max_range': 10000,
+        'quantity': 10,
+        'city': "ADL"
+    },
+    "1017": {
+        'model': 'Man',
+        'capacity': 37000,
+        'max_range': 10000,
+        'quantity': 5,
+        'city': "SYD"
+    },
+    "1018": {
+        'model': 'Man',
+        'capacity': 37000,
+        'max_range': 10000,
+        'quantity': 7,
+        'city': "SYD"
+    },
+    "1019": {
+        'model': 'Man',
+        'capacity': 37000,
+        'max_range': 10000,
+        'quantity': 10,
+        'city': "ADL"
+    },
+    "1020": {
+        'model': 'Man',
+        'capacity': 37000,
+        'max_range': 10000,
+        'quantity': 5,
+        'city': "SYD"
+    },
+    "1021": {
+        'model': 'Man',
+        'capacity': 37000,
+        'max_range': 10000,
+        'quantity': 7,
+        'city': "SYD"
+    },
+    "1022": {
+        'model': 'Man',
+        'capacity': 37000,
+        'max_range': 10000,
+        'quantity': 10,
+        'city': "ADL"
+    },
+    "1023": {
+        'model': 'Man',
+        'capacity': 37000,
+        'max_range': 10000,
+        'quantity': 5,
+        'city': "SYD"
+    },
+    "1024": {
+        'model': 'Man',
+        'capacity': 37000,
+        'max_range': 10000,
+        'quantity': 7,
+        'city': "SYD"
+    },
+    "1025": {
+        'model': 'Man',
+        'capacity': 37000,
+        'max_range': 10000,
+        'quantity': 10,
+        'city': "ADL"
+    },
+    "1026": {
+        'model': 'Actros',
+        'capacity': 26000,
+        'max_range': 13000,
+        'quantity': 5,
+        'city': "SYD"
+    },
+    "1027": {
+        'model': 'Actros',
+        'capacity': 26000,
+        'max_range': 13000,
+        'quantity': 7,
+        'city': "SYD"
+    },
+    "1028": {
+        'model': 'Actros',
+        'capacity': 26000,
+        'max_range': 13000,
+        'quantity': 10,
+        'city': "ADL"
+    },
+    "1029": {
+        'model': 'Actros',
+        'capacity': 26000,
+        'max_range': 13000,
+        'quantity': 5,
+        'city': "SYD"
+    },
+    "1030": {
+        'model': 'Actros',
+        'capacity': 26000,
+        'max_range': 13000,
+        'quantity': 7,
+        'city': "SYD"
+    },
+    "1031": {
+        'model': 'Actros',
+        'capacity': 26000,
+        'max_range': 13000,
+        'quantity': 10,
+        'city': "ADL"
+    },
+    "1032": {
+        'model': 'Actros',
+        'capacity': 26000,
+        'max_range': 13000,
+        'quantity': 5,
+        'city': "SYD"
+    },
+    "1033": {
+        'model': 'Actros',
+        'capacity': 26000,
+        'max_range': 13000,
+        'quantity': 7,
+        'city': "SYD"
+    },
+    "1034": {
+        'model': 'Actros',
+        'capacity': 26000,
+        'max_range': 13000,
+        'quantity': 10,
+        'city': "ADL"
+    },
+    "1035": {
+        'model': 'Actros',
+        'capacity': 26000,
+        'max_range': 13000,
+        'quantity': 5,
+        'city': "SYD"
+    },
+    "1036": {
+        'model': 'Actros',
+        'capacity': 26000,
+        'max_range': 13000,
+        'quantity': 7,
+        'city': "SYD"
+    },
+    "1037": {
+        'model': 'Actros',
+        'capacity': 26000,
+        'max_range': 13000,
+        'quantity': 10,
+        'city': "ADL"
+    },
+    "1038": {
+        'model': 'Actros',
+        'capacity': 26000,
+        'max_range': 13000,
+        'quantity': 5,
+        'city': "SYD"
+    },
+    "1039": {
+        'model': 'Actros',
+        'capacity': 26000,
+        'max_range': 13000,
+        'quantity': 7,
+        'city': "SYD"
+    },
+    "1040": {
+        'model': 'Actros',
+        'capacity': 26000,
+        'max_range': 13000,
+        'quantity': 7,
+        'city': "SYD"
+    }
+
+}
