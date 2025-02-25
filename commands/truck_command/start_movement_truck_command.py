a48a94fcd4696231f092bed3dc01d7e3893554cc
Flazer214 <ivanpustovit786@gmail.com>
2025-02-25 21:28:12 +0200

    start movement

--- commands/truck_command/start_movement_truck_command.py
+++ commands/truck_command/start_movement_truck_command.py
@@ -1,3 +1,7 @@
+import time
+from datetime import datetime, timedelta
+
+from commands.package_command.assign_delivery_package_command import AssignDeliveryPackageCommand
 from core.application_data import ApplicationData
 from commands.helper_command.base_command import BaseCommand
 from commands.helper_command.validate_params_helpers_command import try_parse_int
@@ -11,13 +15,14 @@ class StartMovementTruckCommand(BaseCommand):
     def execute(self, params):
         truck_id = params[0]
         truck = self._app_data.find_truck_by_id(truck_id)
+        print(f"Truck {truck_id} started delivered")
         delivered_count = 0
-        if not truck.assigned_packages:
-            return f"Truck no assigned packages"
-        for package in truck.assigned_packages:
+        for package in self._app_data.packages:
             if package.package_status == PackageStatus.IN_TRANSIT:
-                package.package_status = PackageStatus.DELIVERED
+                package.set_status(PackageStatus.DELIVERED)
                 delivered_count += 1
+            print(f"Truck {truck_id} in transit...")
+            time.sleep(2)
         if delivered_count > 0:
             return f"{delivered_count} package have been successfully delivered by Truck {truck_id}!"
         else:
