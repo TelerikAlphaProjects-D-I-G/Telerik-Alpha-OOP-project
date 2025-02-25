50569f027d8784c4178c778b01e4ca5394b19132
gosho359 <yovchev1909@gmail.com>
2025-02-25 21:01:05 +0200

    Find Trucks in City Command updated

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ class Main:
                         print("1.🏁 Assign Free Truck to Route")
                         print("2.🔍 View Truck Information")
                         print("3. I dont know if works")
-                        print('4. Find Trucks in Certain City:')
+                        print('4.🚚 Find Trucks in Certain City:')
                         print("5.🔙 Back to main menu")
                         truck_choice = input("Enter your choice: ")
 
